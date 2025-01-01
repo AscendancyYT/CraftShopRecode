ade2f03a7f6c6858272adc76aef971d03c31fe43
Single-Phantom <misterminecraftter@gmail.com>
2025-01-01 10:00:29 +0500

    commit

--- script.js
+++ script.js
@@ -16,11 +16,25 @@ function openForm(productName) {
 
 function closeForm() {
     const form = document.getElementById('orderForm');
-    form.classList.add('hidden');
+    form.classList.remove('fade-in');
+    form.classList.add('fade-out')
+    form.classList.add('hidden')
 }
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+qwertyuiopasdfghjklzxcvbnmйцукенгшщзфывапролджэхъячсмитьбюё1234567890';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
 function generateOrderId() {
-    return `ORD-${Math.floor(Math.random() * 1000000)}-ER`;
+    return `ORD-${makeid(3)}-${makeid(3)}-ER`;
 }
 
 async function submitForm(event) {
